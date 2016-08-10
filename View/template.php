733a4111bd0722a74925e22e18e2fc7bc25975f6
Luis Miguel Tolentino Caraballo <luistolentino27@gmail.com>
2016-08-10 01:44:14 -0400

    Quitar el PDF

--- View/template.php
+++ View/template.php
@@ -37,8 +37,6 @@
             <div class="row">
                 <h1 class="thu-real">SISTEMA DE INVENTARIO</h1>
             </div>
-            <a href="/finalinteligencia.pdf">PDF</a>
-
 <?php
         }
 
